bdb77aaaeb8779172443ae564de653fab25f9fcc
Jose Luis Gonzales <joseluisgonzalez@xerintel.es>
2023-11-16 10:17:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'banderas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
